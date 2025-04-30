cf23b1a498f1caa36d0d243cd3f1193d1ff98753
Priyanshu <sonipriyanshu953@gmail.com>
2025-04-30 13:22:37 +0530

    firebase updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -12,6 +12,5 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID 
 };
-console.log(firebaseConfig)
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
